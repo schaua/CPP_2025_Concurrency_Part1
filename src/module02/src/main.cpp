027b607d7e4052704d41274f39ede79d2e065ced
August Schau <schaua@augustschau.com>
2025-06-26 13:50:52 -0400

    Using lock_guard for RAII mutex control

--- src/module02/src/main.cpp
+++ src/module02/src/main.cpp
@@ -10,25 +10,28 @@ std::mutex bal_mut{};
 
 void deposit(double amount){
     std::this_thread::sleep_for(1000ms);
-    bal_mut.lock();
-    current_balance += amount;
-    bal_mut.unlock();
+    {    
+        std::lock_guard<std::mutex> lg{bal_mut};
+        current_balance += amount;
+    }
     std::this_thread::sleep_for(1000ms);
 }
 
 void withdraw(double amount){
     std::this_thread::sleep_for(1000ms);
-    bal_mut.lock();
-    current_balance -= amount;
-    bal_mut.unlock();
+    {    
+        std::lock_guard<std::mutex> lg{bal_mut};
+        current_balance -= amount;
+    }
     std::this_thread::sleep_for(1000ms);
 }
 
 int main(int argc, char const *argv[])
 {
-    bal_mut.lock();
-    std::cout << "Current balance: " << current_balance << std::endl;
-    bal_mut.unlock();
+    {
+        std::lock_guard<std::mutex> lg{bal_mut};
+        std::cout << "Current balance: " << current_balance << std::endl;
+    }
     for(int t = 3; t ; --t){
         // std::thread deposit_thread = std::thread{deposit, 100};
         // std::thread withdraw_thread = std::thread{withdraw, 200};
@@ -37,8 +40,9 @@ int main(int argc, char const *argv[])
         deposit_thread.join();
         withdraw_thread.join();
     }
-    bal_mut.lock();
-    std::cout << "Current balance: " << current_balance << std::endl;
-    bal_mut.unlock();
+    {
+        std::lock_guard<std::mutex> lg{bal_mut};
+        std::cout << "Current balance: " << current_balance << std::endl;
+    }
     return 0;
 }
