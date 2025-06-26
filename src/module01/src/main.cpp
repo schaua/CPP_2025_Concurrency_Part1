6478093d06f8a017a95d1ef8deb40b9ab6cf0beb
August Schau <schaua@augustschau.com>
2025-06-26 11:19:00 -0400

    Using jthread the stop_token

--- src/module01/src/main.cpp
+++ src/module01/src/main.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
 #include <thread>
+#include <chrono>
+
+using namespace std::chrono_literals;
 
 void doWork() {
     std::cout << "I am working..." << std::endl;
@@ -10,20 +13,27 @@ int main(int argc, char const *argv[])
     // std::thread not_a_real_thread{};
 
     auto main_thread_id = std::this_thread::get_id();
+    
+    {
+        // std::thread worker_thread{doWork};
+        auto worker_thread = std::jthread ([](std::stop_token stoken){
+            std::cout << "I am working lambda..." ;
+            while (!stoken.stop_requested()){
+                // repetitive work of the thread 
+                std::cout << ".";
+            }    
+            std::cout << std::endl;
+        });
 
-    // std::thread worker_thread{doWork};
-    std::thread worker_thread([]{
-            std::cout << "I am working lambda..." << std::endl;
-     });
+    // jthread is auto joined when it is destroyed
+    // which is when it goes out scope
+
+    std::this_thread::sleep_for(1000ms);
+
+    worker_thread.request_stop();
+    }
 
     std::cout << "This is the end of the main thread " << main_thread_id << std::endl;
-    
-    // For this simple app there is obviously no call to join or detach
-    // but for more complex functions it might be useful to confirm that
-    // the thread is able to be joined
-    if (worker_thread.joinable()){
-        worker_thread.join();
-    }
 
     return 0;
 }
