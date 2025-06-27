4b7e20e05385b5672a4cec4d29684f929b5a9d6d
August Schau <schaua@augustschau.com>
2025-06-27 09:45:42 -0400

    Add exception transfer

--- src/module04/src/main.cpp
+++ src/module04/src/main.cpp
@@ -2,10 +2,11 @@
 #include <thread>
 #include <chrono>
 #include <exception>
+#include <future>
 
 using namespace std::chrono_literals;
 
-void doWork() {
+void doWork(std::promise<void> prom) {
     try{
     std::cout << "I am working..." << std::endl;
     throw std::runtime_error("Something went wrong");
@@ -13,16 +14,21 @@ void doWork() {
     }
     catch(const std::exception& e)
     {
-        std::cerr << e.what() << '\n';
+        std::cerr << "thread exception" << '\n';
+        prom.set_exception(std::current_exception());
     }
 }
 
 int main(int argc, char const *argv[])
 {
+    std::promise<void> mainPromise;
+    std::future<void> mainFuture = mainPromise.get_future();
+
     std::thread t;
     try
     {
-        t = std::move(std::thread{doWork});
+        t = std::move(std::thread{doWork, std::move(mainPromise)});
+        mainFuture.get();
     }
     catch(const std::exception& e)
     {
